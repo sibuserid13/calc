be2a29bc5f6d7462af7167669b4a0b0a99b882bd
Yaroslav <yaroslav@test.com>
2021-10-16 11:52:38 +0300

    New version

--- calc.sh
+++ calc.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 read file1 < $1
-
-echo $file1 "=" $((file1))
-
-read file2 < $2
-
-echo $file2 "=" $((file2))
-
-echo "Наибольшее значение  из двух файлов:"
-
-if [[ $file1 -ge $file2 ]]; then 
-
-echo $((file1))
-
+if [[ $# -gt 1 ]]; then
+	echo $file1 "=" $((file1))
+	read file2 < $2 ;
+	echo $file2 "=" $((file2))
+	echo "Наибольшее значение  из двух файлов:"
+	if [[ $file1 -ge $file2 ]]; then 
+		echo $((file1))
+	else
+		echo $((file2))
+	fi
 else
-
-echo $((file2))
-
+	echo $file1 "=" $((file1))
 fi
+
